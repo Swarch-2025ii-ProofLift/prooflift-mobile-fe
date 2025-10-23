46e08f6a5cfc28c067bf146caffcf513e90f1151
smxwx <73405701+smxwx@users.noreply.github.com>
2025-10-22 21:57:13 -0500

    Update routes

--- src/API/API_URL.js
+++ src/API/API_URL.js
@@ -1,6 +1,6 @@
 import { API_BASE_URL } from '@env';
 
-export const API_URL = `http://${API_BASE_URL}:8000`;
+export const API_URL = `http://${API_BASE_URL}:8000/auth`;
 export const API_URL_SUGGEST = `http://${API_BASE_URL}:8000/suggest`;
 
 console.log("API_URL usado is this one:", API_URL);
